3ef4ef5c58aafd885ec9846498776b8869e29396
Sviridov Dmitry <sviridov@neolant.ru>
2012-01-10 18:08:38 +0400

    Созданы конструкторы для дальнейшей реализации

--- src/main/java/ru/aristar/jnuget/rss/PackageEntry.java
+++ src/main/java/ru/aristar/jnuget/rss/PackageEntry.java
@@ -4,6 +4,8 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import ru.aristar.jnuget.files.NupkgFile;
+import ru.aristar.jnuget.files.NuspecFile;
 
 /**
  *
@@ -15,6 +17,17 @@ public class PackageEntry {
 
     @XmlElement(name = "id")
     private String id;
+
+    public PackageEntry() {
+    }
+
+    public PackageEntry(NupkgFile nupkgFile) {
+        this(nupkgFile.getNuspecFile());
+    }
+
+    public PackageEntry(NuspecFile nuspecFile) {
+        throw new UnsupportedOperationException("Конструктор не реализован");
+    }
 
     public String getId() {
         return id;
