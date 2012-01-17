a43e3a0addf8565ab7e51492058f60beb921b5ff
Sviridov Dmitry <sviridov@neolant.ru>
2012-01-17 17:44:45 +0400

    Реализована примитивная версия построителя запрсов

--- src/main/java/ru/aristar/jnuget/QueryExecutor.java
+++ src/main/java/ru/aristar/jnuget/QueryExecutor.java
@@ -1,8 +1,10 @@
 package ru.aristar.jnuget;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import ru.aristar.jnuget.files.NupkgFile;
-import ru.aristar.jnuget.sources.FilePackageSource;
+import ru.aristar.jnuget.sources.PackageSource;
 
 /**
  *
@@ -10,7 +12,23 @@ import ru.aristar.jnuget.sources.FilePackageSource;
  */
 class QueryExecutor {
 
-    public Collection<NupkgFile> execQuery(FilePackageSource packageSource, final String filter) {
+    public Collection<NupkgFile> execQuery(PackageSource packageSource, final String filter) {
+        if (filter == null || "".equals(filter)) {
+            return packageSource.getPackages();
+        }
+        if (filter.toLowerCase().startsWith("tolower(id) eq")) {
+            Pattern pattern = Pattern.compile("(\\w+)\\((\\w+)\\)\\s+(\\w+)\\s+(.+)");
+            Matcher matcher = pattern.matcher(filter);
+            if (matcher.find()) {
+                String function = matcher.group(1);
+                String field = matcher.group(2);
+                String condition = matcher.group(3);
+                String value = matcher.group(4);
+                return packageSource.getPackages(value, true);
+            }
+        } else if (filter.toLowerCase().startsWith("islatestversion")) {
+            return packageSource.getLastVersionPackages();
+        }
         return packageSource.getPackages();
     }
 }
