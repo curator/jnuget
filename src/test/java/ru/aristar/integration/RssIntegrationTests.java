abaffac34ac41f7f84651f72f34456a25aacf9ed
Sviridov D.V <aristar@mail.ru>
2012-01-23 22:41:39 +0400

    Исправлен запуск интеграционных тестов

--- src/test/java/ru/aristar/integration/RssIntegrationTests.java
+++ src/test/java/ru/aristar/integration/RssIntegrationTests.java
@@ -1,11 +1,8 @@
 package ru.aristar.integration;
 
-import com.meterware.httpunit.PostMethodWebRequest;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebResponse;
-import org.junit.After;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -15,38 +12,17 @@ import org.junit.Test;
 public class RssIntegrationTests {
 
     /**
-     * good XML input
-     */
-    private static final String XML = "./src/test/resources/xml/file.xml";
-    /**
-     * URL
-     */
-    private static final String URL = "http://localhost:8088";
-    private PostMethodWebRequest req;
-    private WebConversation wc;
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() {
-        req = new PostMethodWebRequest(URL);
-        wc = new WebConversation();
-    }
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
-     * Test method for {@link biz.zenoconsulting.jersey.MyServlet#doGet()}.
+     * Корневой URL содержит страницу с общей информацией
+     *
+     * @throws Exception ошибка в процессе теста
      */
     @Test
     public void testDoGet() throws Exception {
-        WebResponse wr = wc.getResponse(URL);
-        assertTrue(wr.getText().contains("hello"));
+        //GIVEN
+        WebConversation webConversation = new WebConversation();
+        //WHEN
+        WebResponse response = webConversation.getResponse("http://localhost:8088");
+        //THEN
+        assertTrue(response.getText().contains("You are running JNuGet.Server"));
     }
 }
