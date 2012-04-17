c419050440b8aef34b9d689876252d991b1787e7
Sviridov D.V <aristar@mail.ru>
2012-04-17 21:40:26 +0400

    Реализация условий поиска в хранилище

--- src/main/java/ru/aristar/jnuget/QueryExecutor.java
+++ src/main/java/ru/aristar/jnuget/QueryExecutor.java
@@ -1,5 +1,6 @@
 package ru.aristar.jnuget;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,12 +14,32 @@ import ru.aristar.jnuget.sources.PackageSource;
 class QueryExecutor {
 
     //TODO filter=((((((tolower(Id) eq 'neolant.projectwise.api') or (tolower(Id) eq 'neolant.projectwise.api')) or (tolower(Id) eq 'neolant.projectwise.controls')) or (tolower(Id) eq 'neolant.projectwise.isolationlevel')) or (tolower(Id) eq 'neolant.projectwise.isolationlevel.implementation')) or (tolower(Id) eq 'nlog')) or (tolower(Id) eq 'postsharp') and isLatestVersion
-    //TODO Добавить поддержку searchTerm='nug'
-    
+    /**
+     * Получение списка пакетов из хранилиза
+     *
+     * @param packageSource хранилище пакетов
+     * @param filter фильтр пакетов
+     * @param searchTerm условие поиска
+     * @return коллекция пакетов
+     */
+    public Collection<Nupkg> execQuery(PackageSource<Nupkg> packageSource, final String filter, final String searchTerm) {
+        Collection<Nupkg> nupkgs = execQuery(packageSource, filter);
+        if (searchTerm == null || searchTerm.equals("")) {
+            return nupkgs;
+        }
+        ArrayList<Nupkg> result = new ArrayList<>();
+        for (Nupkg nupkg : nupkgs) {
+            if (nupkg.getId().toLowerCase().contains(searchTerm)) {
+                result.add(nupkg);
+            }
+        }
+        return result;
+    }
+
     public Collection<Nupkg> execQuery(PackageSource<Nupkg> packageSource, final String filter) {
         if (filter == null || "".equals(filter)) {
             return packageSource.getPackages();
-        }        
+        }
         if (filter.toLowerCase().startsWith("tolower(id) eq")) {
             Pattern pattern = Pattern.compile("(\\w+)\\((\\w+)\\)\\s+(\\w+)\\s+'?([^']+)'?");
             Matcher matcher = pattern.matcher(filter);
--- src/test/java/ru/aristar/jnuget/QueryExecutorTest.java
+++ src/test/java/ru/aristar/jnuget/QueryExecutorTest.java
@@ -1,21 +1,31 @@
 package ru.aristar.jnuget;
 
+import java.util.Arrays;
+import java.util.Collection;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import ru.aristar.jnuget.files.Nupkg;
 import ru.aristar.jnuget.sources.PackageSource;
+import static org.jmock.Expectations.*;
+import ru.aristar.jnuget.files.NugetFormatException;
 
 /**
+ * Тест класса, ограничивающего выборку пакетов
  *
  * @author sviridov
  */
 public class QueryExecutorTest {
 
+    /**
+     * Контекст создания заглушек
+     */
     private Mockery context = new Mockery();
 
     @Test
-    public void testExecQueryWithNull() throws Exception {
+    public void testExecQueryWithNull() {
         //GIVEN
         @SuppressWarnings("unchecked")
         final PackageSource<Nupkg> source = context.mock(PackageSource.class);
@@ -33,7 +43,7 @@ public class QueryExecutorTest {
     }
 
     @Test
-    public void testExecQueryWithId() throws Exception {
+    public void testExecQueryWithId() {
         //GIVEN
         final String filter = "tolower(id) eq 'package.name'";
         @SuppressWarnings("unchecked")
@@ -52,7 +62,7 @@ public class QueryExecutorTest {
     }
 
     @Test
-    public void testExecQueryWithLastVersion() throws Exception {
+    public void testExecQueryWithLastVersion() {
         //GIVEN
         final String filter = "isLatestVersion";
         @SuppressWarnings("unchecked")
@@ -68,5 +78,56 @@ public class QueryExecutorTest {
         executor.execQuery(source, filter);
         //THEN
         context.assertIsSatisfied();
+    }
+
+    /**
+     * Проверка поиска версии с условием поиска
+     *
+     * @throws NugetFormatException тестовый формат версии не соответствует
+     * формату
+     */
+    @Test
+    public void testExecQueryWithSearchTerm() throws NugetFormatException {
+        //GIVEN
+        QueryExecutor executor = new QueryExecutor();
+        final String searchTerm = "id";
+        @SuppressWarnings("unchecked")
+        final PackageSource<Nupkg> source = context.mock(PackageSource.class);
+        context.checking(new Expectations() {
+
+            {
+                atLeast(0).of(source).getPackages();
+                will(returnValue(Arrays.asList(
+                        createPackageStub("id1", "1.0.1"),
+                        createPackageStub("aaaa", "1.2.3"))));
+            }
+        });
+        //WHEN
+        Collection<Nupkg> result = executor.execQuery(source, null, searchTerm);
+        Nupkg[] nupkgs = result.toArray(new Nupkg[0]);
+        //THEN
+        assertThat("Количество отфильтрованных пакетов", nupkgs.length, is(equal(1)));
+        assertThat("Идентификатор пакета", nupkgs[0].getId(), is(equalTo("id1")));
+        assertThat("Версия пакета", nupkgs[0].getVersion(), is(equalTo(Version.parse("1.0.1"))));
+    }
+
+    /**
+     * Создание заглушки пакета
+     *
+     * @param id идентификатор пакета
+     * @param version версия пакета
+     * @return заглушка пакета
+     * @throws NugetFormatException версиz не соответствует формату
+     */
+    private Nupkg createPackageStub(String id, String version) throws NugetFormatException {
+        Nupkg result = context.mock(Nupkg.class, id + ":" + version);
+        Expectations expectations = new Expectations();
+        expectations.atLeast(0).of(result).getId();
+        expectations.will(returnValue(id));
+        expectations.atLeast(0).of(result).getVersion();
+        expectations.will(returnValue(Version.parse(version)));
+        context.checking(expectations);
+        return result;
+
     }
 }
