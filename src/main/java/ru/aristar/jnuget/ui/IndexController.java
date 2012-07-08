e1ae89c0d8715ee345c8c0f07a094b2e5d0ab018
Sviridov D.V <aristar@mail.ru>
2012-07-09 00:36:09 +0400

    Рефакторинг, комментарии

--- src/main/java/ru/aristar/jnuget/ui/IndexController.java
+++ src/main/java/ru/aristar/jnuget/ui/IndexController.java
@@ -8,11 +8,11 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Контроллер для индексной страницы
  *
  * @author sviridov
  */
@@ -20,12 +20,22 @@ import org.slf4j.LoggerFactory;
 @ApplicationScoped
 public class IndexController {
 
+    /**
+     * Логгер
+     */
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    /**
+     * @return контекст приложения в сервере
+     */
     private ExternalContext getContext() {
         return FacesContext.getCurrentInstance().getExternalContext();
     }
 
+    /**
+     * @return URI приложения на сервере
+     * @throws URISyntaxException ошибка получения URI прилодения
+     */
     private URI getApplicationUri() throws URISyntaxException {
         ExternalContext context = getContext();
         URI uri = new URI(context.getRequestScheme(),
@@ -38,25 +48,37 @@ public class IndexController {
         return uri;
     }
 
+    /**
+     * @return версия приложения
+     */
     public String getVersion() {
-        String vesion = null;
         try {
             InputStream inputStream = getContext().getResourceAsStream("/META-INF/MANIFEST.MF");
             if (inputStream != null) {
                 Manifest manifest = new Manifest(inputStream);
-                vesion = manifest.getMainAttributes().getValue("Implementation-Version");
+                String vesion = manifest.getMainAttributes().getValue("Implementation-Version");
+                return "v" + vesion;
+            } else {
+                return "";
             }
         } catch (Exception e) {
             logger.warn("Ошибка определения версии пакета", e);
+            return "";
         }
-        vesion = vesion == null ? "" : "v" + vesion;
-        return vesion;
     }
 
+    /**
+     * @return URL, по которому можно получать пакеты
+     * @throws URISyntaxException ошибка получения URI прилодения
+     */
     public String getStrorageGetUrl() throws URISyntaxException {
         return getApplicationUri().toASCIIString() + "/nuget/nuget";
     }
 
+    /**
+     * @return URL, по которому можно публиковать пакеты
+     * @throws URISyntaxException ошибка получения URI прилодения
+     */
     public String getStroragePutUrl() throws URISyntaxException {
         return getApplicationUri().toASCIIString() + "/nuget/";
     }
