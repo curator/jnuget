1b5b60081dad3799d903998f7f528fd36e49c673
sviridov <none@none>
2012-07-25 14:04:24 +0400

    Доработана спецификация Подправлен UI

--- src/test/groovy/ru/aristar/jnuget/spec/RoleSpec.groovy
+++ src/test/groovy/ru/aristar/jnuget/spec/RoleSpec.groovy
@@ -5,21 +5,29 @@ import ru.aristar.jnuget.security.Role
 
 /**
  * Спецификация ролей пользователей
- *   
+ *
  * @author sviridov
  */
 class RoleSpec extends spock.lang.Specification{
 
-   /**
-    * Роли могут включать в себя другие роли
-    */
+    /**
+     * Роли могут включать в себя другие роли
+     */
     def "Roles can include other roles"(){
         expect:
         Role.valueOf(parentRole).contains(Role.valueOf(childRole)) == contains
         where:
-        parentRole     |    childRole    |  contains  
-        "Administrator"| "Administrator" |    true  
-        //TODO Дописать спецификацию
+        parentRole     |    childRole    |  contains
+        "Administrator"| "Administrator" |    true
+        "Administrator"| "GuiUser"       |    true
+        "Administrator"| "Push"          |    true
+        "Administrator"| "Read"          |    true
+        "Administrator"| "Delete"        |    true
+        "Administrator"| "Delete"        |    true
+        "GuiUser"      | "Administrator" |    false
+        "GuiUser"      | "Delete"        |    false
+        "GuiUser"      | "Read"          |    false
+        "GuiUser"      | "Push"          |    false
     }
 }
 
